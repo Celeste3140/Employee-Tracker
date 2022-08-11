7a85f9fae2a382bd41b3221ff28995234f809542
carambola <celestebaltazar@yahoo.com>
2022-08-11 13:03:32 -0700

    added choice options

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+
+function questions() {
+inquirer.prompt ({
+    name: 'choice',
+    message: 'Select an option!',
+    choices: 
+    [
+        'View All Employees',
+        'Add Employee',
+        'Update Employee Role',
+        'View All Roles',
+        'View All Departments',
+        'Add Department'
+    ]
+})
+}
